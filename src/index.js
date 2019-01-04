e5938d03b9a9462e7b5173790adbcaf1f20c83ee
nienie0714 <niexinyu0714@163.com>
2019-01-04 10:27:05 +0800

    函数声明组件（另一种是class类），跑通路由

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom'
+import {
+  HashRouter as Router, // 容器
+  Route, // 一条路由
+} from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/**
+ * 跑通路由
+ */
+function Root() {
+  return <h1>Root</h1>
+}
+function Home() {
+  return <h1>Home</h1>
+}
+function User() {
+  return <h1>User</h1>
+}
+function Profile() {
+  return <h1>Profile</h1>
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <Router>
+    <div>
+      <Route path="/" component={Root}></Route>
+      <Route path="/home" component={Home}></Route>
+      <Route path="/user" component={User}></Route>
+      <Route path="/profile" component={Profile}></Route>
+    </div>
+  </Router>
+  , document.getElementById('root')
+)
